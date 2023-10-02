b4b5deeeef3be5ccce548ec02d55fcd8e1423788
Gabriel "Syll3x" Ponzoni <109359526+GabrielPonzoni@users.noreply.github.com>
2023-10-02 10:46:57 -0300

    Instanciated Objects and Runed Metods

--- ScreenSound2.0/ScreenSound2.0/Musica.cs
+++ ScreenSound2.0/ScreenSound2.0/Musica.cs
@@ -5,5 +5,17 @@
     public int duracao;
     public bool disponivel;
 
-
+    public void ExibirFichaTecnica() // metodo sem retorno > void, dentro de um método colocamos instruções
+    {
+        Console.WriteLine($"Nome da Música: {nome}");
+        Console.WriteLine($"Nome do artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel) 
+        {
+            Console.WriteLine("Disponível no plano");
+        } else 
+        {
+            Console.WriteLine("Adquira o plano Plus+"); 
+        }
+    }
 }
--- ScreenSound2.0/ScreenSound2.0/Program.cs
+++ ScreenSound2.0/ScreenSound2.0/Program.cs
@@ -2,13 +2,14 @@
 Musica musica1 = new Musica();
 musica1.nome = "Roxane";
 musica1.artista = "The police";
-
-Console.WriteLine($"Nome da Música: {musica1.nome}");
-Console.WriteLine($"Nome do artista: {musica1.artista}");
+musica1.duracao = 273;
+musica1.disponivel = true;
+musica1.ExibirFichaTecnica();
 
 Musica musica2 = new Musica();
 musica2.nome = "Vertigo";
 musica2.artista = "U2";
+musica2.duracao = 367;
+musica2.disponivel = false;
+musica2.ExibirFichaTecnica();
 
-Console.WriteLine($"Nome da Música: {musica2.nome}");
-Console.WriteLine($"Nome do artista: {musica2.artista}");
